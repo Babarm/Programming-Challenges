90ec66196f9993343a9e30895a1f8f485663e5a0
Babarm <kevin@DESKTOP-170SURH.localdomain>
2018-05-24 20:42:48 -0700

    initial commit

---
+++ Project Euler/Python/template.py
@@ -0,0 +1,23 @@
+import time
+
+def run():
+	ans = 0
+	return ans
+
+
+challengeID = 0
+challengeName = ""
+
+print("#" + str(challengeID) + ": " + challengeName)
+input("Press ENTER to run challenge . . . ")
+print("")
+
+start = time.time()
+
+ans = run()
+
+end = time.time()
+
+print("Answer: {0:,d}".format(ans))
+
+print("Completed in: {0:.3f}".format(end - start) + " sec")
